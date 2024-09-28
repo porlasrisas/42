1a30b1f1b644c1e1f6051feb88755c4b7963a95a
guigonzal <guigonza@c1r12s5.42madrid.com>
2024-09-28 19:02:18 +0200

    todas bien

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: guigonza <guigonza@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 13:17:13 by guigonza          #+#    #+#             */
-/*   Updated: 2024/09/27 11:52:25 by guigonza         ###   ########.fr       */
+/*   Updated: 2024/09/27 19:50:21 by guigonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,7 +58,16 @@ size_t	ft_strlen(const char *s);
 
 typedef struct s_list
 {
-	void *content;
-	struct s_list *next;
-} t_list;
+	void			*content;
+	struct s_list	*next;
+}	t_list;
+t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void*));
+void	ft_lstclear(t_list	**lst, void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 #endif
